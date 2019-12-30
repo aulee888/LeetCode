e16df15faf03790a942df6459f244e91cb71637d
Austin <58223643+aulee888@users.noreply.github.com>
2019-12-30 07:36:09 -0800

    Create Valid Parentheses.py
    Problem 20

---
+++ Valid Parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        paren = {'(': ')', '[': ']', '{': '}'}
+        valid = True
+        char = []
+
+        for i in range(len(s)):
+            if s[i] in '({[':
+                char.append(s[i])
+
+            else:
+                if char == []:
+                    valid = False
+                else:
+                    if paren[char.pop()] != s[i]:
+                        valid = False
+
+                        break
+        if char != []:
+            valid = False
+
+        return valid
