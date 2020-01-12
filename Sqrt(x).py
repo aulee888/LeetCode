7547cb820ed7662d3462895938f7a9fd61a8d353
aulee888 <aulee5451@gmail.com>
2020-01-12 13:40:27 -0800

    Fixed memory crashing from large inputs

--- Sqrt(x).py
+++ Sqrt(x).py
@@ -1,24 +1,26 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
 
-        def binary_search(alist, first, last):
-            """Only works for values greater than 2 due to list creation"""
+        def binary_search(first, last):
+            """Only works for values greater than 2 due to list creation.
+            Avoids using a list which uses too much memory for large numbers.
+            """
             mid = (first + last) // 2
 
-            if alist[mid] ** 2 == x:
-                return alist[mid]
+            if mid ** 2 == x:
+                return mid
 
-            elif alist[mid] ** 2 > x:
-                if alist[mid - 1] ** 2 < x:
-                    return alist[mid - 1]
+            elif mid ** 2 > x:
+                if (mid - 1) ** 2 < x:
+                    return mid - 1
                 else:
-                    return binary_search(alist, first, mid)
+                    return binary_search(first, mid)
 
-            elif alist[mid] ** 2 < x:
-                if alist[mid + 1] ** 2 > x:
-                    return alist[mid]
+            elif mid ** 2 < x:
+                if (mid + 1) ** 2 > x:
+                    return mid
                 else:
-                    return binary_search(alist, first + 1, last)
+                    return binary_search(mid + 1, last)
 
         if x == 0:
             return 0
@@ -27,5 +29,4 @@ class Solution:
             return 1
 
         else:
-            alist = [i for i in range(0, x)]
-            return binary_search(alist, 0, x)
+            return binary_search(0, x)
