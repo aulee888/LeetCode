9107d4e2641a095ebade5d109e667d530319367f
aulee888 <aulee5451@gmail.com>
2020-01-14 09:09:16 -0800

    Created working recursive solution and added two point solution

--- Two Sum II.py
+++ Two Sum II.py
@@ -1,12 +1,16 @@
 class Solution:
-    def twoSum(self, numbers: list, target: int) -> list:
-        """Implements a recursive binary search"""
+    def rec_twoSum(self, numbers: list, target: int) -> list:
+        """Recursive binary search
+        Time Complexity: O(n*log(n))
+        """
         def binary_search(pos, first, last):
             mid = (first + last) // 2
-            print('First: {}, Mid: {}, Last: {}, Value: {}'.format(first, mid, last, numbers[mid]))
 
-            #if first == last:
-            #    return False
+            if mid == last:  # Base case
+                if numbers[pos] + numbers[mid] == target:
+                    return [pos + 1, mid + 1]
+                else:
+                    return False
 
             if numbers[pos] + numbers[mid] == target:
                 return [pos+1, mid+1]  # Accounts for non-zero index
@@ -20,13 +24,23 @@ class Solution:
         n = len(numbers)
 
         for i in range(n):
-            print(i)
-            print('---')
             result = binary_search(i, i+1, n-1)
-            print('')
             if result:
                 return result
 
+    def it_twoSum(self, numbers: list, target: int) -> list:
+        """Two Point solution
+        Runs in half the time a recursive binary search takes on LeetCode
+        Time complexity: O(n)
+        """
+        i = 0
+        j = len(numbers) - 1
+        while i <= j:
+            if numbers[i] + numbers[j] == target:
+                return [i+1, j+1]
 
-l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-print(Solution().twoSum(l1, 27))
+            elif numbers[i] + numbers[j] < target:
+                i += 1
+
+            elif numbers[i] + numbers[j] > target:
+                j -= 1
