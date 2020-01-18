630efc838120023f33e9beedb10fe2c3cf48f09e
aulee888 <aulee5451@gmail.com>
2020-01-18 12:52:26 -0800

    Updated hash table solution and added pointer solution

--- Linked List Cycle.py
+++ Linked List Cycle.py
@@ -6,17 +6,43 @@
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
+        """Hash table solution.
+        Appends head instead of head.val because head records different
+        objects. It is a solution to storing the same value to seen and
+        having the same value show up in a different object.
+        Time Complexity: O(n), Space Complexity: O(n)
+        """
         seen = []
 
-        if not head:  # Accounts for lists on size 1
-            return False
-
-        while head and head.next:  # Must be while head because while head.val returns wrong output on 0
-            if (head.val, head.next.val) in seen:
+        while head:
+            if head in seen:
                 return True
             else:
-                seen.append((head.val, head.next.val))
+                seen.append(head)  # Append head instead of head.val
 
             head = head.next
 
         return False
+
+    def hasCycle(self, head: ListNode) -> bool:
+        """Two pointer solution
+        Pointers cannot be moving down lists at same speed otherwise they
+        would never equal each other in a cycle.
+        (i.e pointer2 will always be 1 ahead)
+        Time Complexity: O(n), Space Complexity: O(1)
+        """
+        if not head:
+            return False
+
+        pointer1 = head
+        pointer2 = head.next
+
+        while pointer1 != pointer2:
+            if not pointer2 or not pointer2.next:  # If pointer2 end of list
+                return False
+
+            pointer1 = pointer1.next
+            pointer2 = pointer2.next.next
+
+        return True
+
