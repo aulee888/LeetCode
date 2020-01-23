5952aa9d3fb4b1c2eebb261d11500e349d6d9bba
aulee888 <aulee5451@gmail.com>
2020-01-23 10:43:14 -0800

    Removed divide and conquer attempt. Will update when more learned

--- Maximum Subarray.py
+++ Maximum Subarray.py
@@ -1,39 +1,12 @@
 class Solution:
-    def maxSubArray1(self, nums: list) -> int:
-        """Attempt at a divide and conquer solution"""
-        def rec_helper(first, last):
-            if first == last:
-                return first
-
-            mid = (first + last) // 2
-
-            left = rec_helper(first, mid)
-            right = rec_helper(mid + 1, last)
-
-            count = 0
-            for i in range((last + 1) - first):
-                count += nums[left + i]
-
-            for j in range(first, last + 1):
-                if nums[first] > count:
-                    pass
-
-
-            if (left + right) > left and (left + right) > right:
-                return left + right
-
-            elif left > right:
-                return left
-
-            else:
-                return right
-
     def maxSubArray(self, nums):
         n = len(nums)
         curr_sum = nums[0]
         max_sum = nums[0]
 
         for i in range(1, n):
+            # Even if all nums were < 0, adding negatives makes a more
+            # negative number
             if curr_sum < 0:
                 curr_sum = nums[i]
 
